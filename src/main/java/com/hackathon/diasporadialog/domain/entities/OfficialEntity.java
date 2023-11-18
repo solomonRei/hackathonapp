7c335d7188238844a80ec8c79b29bba244f3e748
Andrei Cernisov <andrei.cernisov@isa.utm.md>
2023-11-18 23:20:13 +0200

    add relations and cascase types to entities

--- src/main/java/com/hackathon/diasporadialog/domain/entities/OfficialEntity.java
+++ src/main/java/com/hackathon/diasporadialog/domain/entities/OfficialEntity.java
@@ -1,16 +1,9 @@
 package com.hackathon.diasporadialog.domain.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
 
 @Builder
 @Getter
@@ -34,6 +27,10 @@ public class OfficialEntity {
     @Column
     private String bio;
 
-    @Column(name = "user_id")
-    private Long userId;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private UserEntity user;
+
+    @OneToOne(mappedBy = "official", cascade = CascadeType.ALL)
+    private MeetingEntity meeting;
 }
--- src/main/java/com/hackathon/diasporadialog/domain/entities/QuestionEntity.java
+++ src/main/java/com/hackathon/diasporadialog/domain/entities/QuestionEntity.java
@@ -1,16 +1,7 @@
 package com.hackathon.diasporadialog.domain.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -36,11 +27,13 @@ public class QuestionEntity {
     @Column(name = "vote_count")
     private Integer voteCount;
 
-    @Column(name = "meeting_id")
-    private Long meetingId;
+    @ManyToOne
+    @JoinColumn(name = "meeting_id")
+    private MeetingEntity meeting;
 
-    @Column(name = "user_id")
-    private Long userId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
