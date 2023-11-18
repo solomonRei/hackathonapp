3552a39fba2710f949ae22fbbc558fe6dfc5e175
Eduard <dz6class@mail.ru>
2023-11-18 13:58:10 +0200

    first commit

---
+++ src/main/java/com/hackathon/diasporadialog/DiasporadialogApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.diasporadialog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiasporadialogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiasporadialogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/diasporadialog/DiasporadialogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.diasporadialog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiasporadialogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
